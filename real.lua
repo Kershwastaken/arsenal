c6462867739fa14c4f125e80db765366ed6794b9
Thanosdagamer <106916828+Thanosdagamer@users.noreply.github.com>
2022-07-05 19:01:59 -0400

    Update real.lua

--- real.lua
+++ real.lua
@@ -1,6 +1,5 @@
- Size = 8 -- Setting higher than 8 or so will screw with the server hit detection and prevent your guns from damaging people. 8 is still easy to "rage" with. I recommend 2-5 if you want to look legit.
-Transparency = 0.5
-
+Size = 8 -- Setting higher than 8 or so will screw with the server hit detection and prevent your guns from damaging people. 8 is still easy to "rage" with. I recommend 2-5 if you want to look legit.
+Transparency = 0.5 -- Leave it at 0.5 if you want the heads to be visible. Set to 1 to make them invisible.
 
 game:GetService("RunService").Stepped:Connect(function()
 for i,v in next, workspace.Players:GetDescendants() do
@@ -26,7 +25,4 @@ while wait() do
 for i,v in next, workspace.Ignore.DeadBody:GetChildren() do
 v:Destroy()
 end
-end
-end)
-
 end
